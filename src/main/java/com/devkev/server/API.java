7cc1ff0d4e1390882a6625a34a9d0ff2952cab5e
DevKevYT <devkev@ddbot.top>
2023-10-09 14:37:29 +0200

    Version 1.0.3

--- src/main/java/com/devkev/server/API.java
+++ src/main/java/com/devkev/server/API.java
@@ -30,7 +30,7 @@ import com.devkev.server.Match.MatchLeaveReasons;
 
 public class API extends Jooby {
 	
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 	
 	ScheduledExecutorService deleteExpiredClients = Executors.newScheduledThreadPool(1);
 	
