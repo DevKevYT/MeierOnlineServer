eeeb1df84927a373b967c27df6b4572d6ce85019
Philipp <Philipp@DESKTOP-PHILIPP>
2023-11-05 12:56:47 +0100

    Merge branch 'main' of https://github.com/DevKevYT/MeierOnlineServer.git into main

--- src/main/java/com/devkev/server/API.java
+++ src/main/java/com/devkev/server/API.java
@@ -30,7 +30,7 @@ import com.devkev.server.Match.MatchLeaveReasons;
 
 public class API extends Jooby {
 	
-	public static final String VERSION = "Beta 1.0.7";
+	public static final String VERSION = "Beta 1.0.8";
 	
 	ScheduledExecutorService deleteExpiredClients = Executors.newScheduledThreadPool(1);
 	
